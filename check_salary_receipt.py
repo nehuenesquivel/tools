360e7cf87a22133f197a5d6a6b57763bd54d451c
nehuenesquivel <nehuenesquivel@outlook.com>
2024-09-12 12:34:27 -0300

    duplicated print deleted

--- check_salary_receipt.py
+++ check_salary_receipt.py
@@ -16,16 +16,17 @@ def write_file(dict):
     file.close()
 
 
+salary_in_usd = float(input("Salary in USD: "))
+usd_value_in_ars = float(input("USD value in ARS: "))
+
 dict = dict()
-dict["Salary in USD"] = float(input("Salary in USD: "))
-dict["USD value in ARS"] = float(input("USD value in ARS: "))
-dict["Salary in ARS"] = dict["Salary in USD"] * dict["USD value in ARS"]
+dict["Salary in ARS"] = salary_in_usd * usd_value_in_ars
 dict["SIJP"] = dict["Salary in ARS"] * 0.11
 dict["INSSJP"] = dict["Salary in ARS"] * 0.03
 dict["OSOC"] = dict["Salary in ARS"] * 0.03
 dict["Deductions"] = dict["SIJP"] + dict["INSSJP"] + dict["OSOC"]
 dict["Net"] = dict["Salary in ARS"] - dict["Deductions"]
 dict["Net in ARS"] = dict["Net"] * 0.3
-dict["Net in USD"] = dict["Net"] * 0.7 / dict["USD value in ARS"]
+dict["Net in USD"] = dict["Net"] * 0.7 / usd_value_in_ars
 
 print_screen(dict)
