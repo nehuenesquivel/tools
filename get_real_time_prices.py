810a38c684369d4b76515ccfc99b64d002a1bc12
nehuenesquivel <nehuenesquivel@outlook.com>
2025-05-13 16:38:33 -0300

    i'm done

--- get_real_time_prices.py
+++ get_real_time_prices.py
@@ -4,152 +4,139 @@ import cv2
 import numpy as nm
 import time
 import datetime
-import random
+import winsound
 
 pytesseract.pytesseract.tesseract_cmd = "D:/Technologies/Tesseract-OCR/tesseract.exe"
 
-options = {
-    "al30_sell": {"enabled": True, "current_value": 0, "previous_value": 0, "coordinates": [1275, 446, 1370, 463], "position": 1},
-    "al30c_sell": {"enabled": False, "current_value": 0, "previous_value": 0,"coordinates": [1275, 508, 1370, 525], "position": 1},
-    "al30d_sell": {"enabled": False, "current_value": 0, "previous_value": 0,"coordinates": [1275, 570, 1370, 587], "position": 1},
-    "al30_buy": {"enabled": False, "current_value": 0, "previous_value": 0,"coordinates": [1375, 446, 1470, 463], "position": 0},
-    "al30c_buy": {"enabled": True, "current_value": 0, "previous_value": 0,"coordinates": [1375, 508, 1470, 525], "position": 0},
-    "al30d_buy": {"enabled": True, "current_value": 0, "previous_value": 0,"coordinates": [1375, 570, 1470, 587], "position": 0},
+chars_for_split = ["x", "=", "<", ">", "«", "*", "~"]
+
+al30_sell = {
+    "name": "al30_sell",
+    "current_price": 0,
+    "previous_price": 0,
+    "current_value": 0,
+    "previous_value": 0,
+    "coordinates": [1275, 446, 1370, 463],
+    "position": 1,
+}
+al30d_sell = {
+    "name": "al30d_sell",
+    "current_price": 0,
+    "previous_price": 0,
+    "current_value": 0,
+    "previous_value": 0,
+    "coordinates": [1275, 570, 1370, 587],
+    "position": 1,
+}
+al30c_buy = {
+    "current_value": 0,
+    "previous_value": 0,
+    "coordinates": [1375, 508, 1470, 525],
+    "position": 0,
 }
 
-chars_for_split = ["x", "=", "<", ">", "«", "*"]
 
-def get_enabled_options():
-    enabled_options = dict()
-    for option, properties in options.items():
-        if properties["enabled"]:
-            enabled_options[option] = properties
-    #print(str(enabled_options))
+def update_values():
+    update_option_values(al30_sell)
+    update_option_values(al30d_sell)
+    update_option_values(al30c_buy)
 
-def update_enabled_options_property_current_value():
-    for properties in enabled_options.values():
-        #print(properties["current_value"])
-        update_property_current_value(properties)
-        #print(properties["current_value"])
 
-def update_property_current_value(properties):
+def update_option_values(option):
     string = ""
     while string == "":
-        image = ImageGrab.grab(bbox=properties["coordinates"])
-    # image.show()
-
-        string = (
-            pytesseract.image_to_string(
-                cv2.cvtColor(nm.array(image), cv2.COLOR_BGR2GRAY), lang="eng"
-            ).strip()
-        )
-    #print("string: " + string)
+        image = ImageGrab.grab(bbox=option["coordinates"])
+        # image.show()
+        string = pytesseract.image_to_string(
+            cv2.cvtColor(nm.array(image), cv2.COLOR_BGR2GRAY), lang="eng"
+        ).strip()
+        # print("string: " + string)
 
     string_formatted = format_string(string)
+    try:
+        option["previous_value"] = option["current_value"]
+        option["current_value"] = float(
+            string_formatted.split(get_split_char(string_formatted))[option["position"]]
+        )
+        # print("option["current_value"]: " + option["current_value"])
+    except Exception as exception:
+        print("update exception: ", exception)
+        print("* resuming *")
 
-    properties["current_value"] = string_formatted.split(get_split_char(string_formatted))[properties["position"]].strip()
-    # print("properties["current_value"]: " + properties["current_value"])
 
 def format_string(string):
-    string = string.replace(",", ".")
-
+    string = string.replace(",", ".").replace(":", ".")
     if string[-1] == ".":
         string = string[:-1]
-    
     return string
+
 
 def get_split_char(string):
     for char in chars_for_split:
         if char in string:
             return char
 
-def print_useful_values():
-    useful_values = dict()
-    for option, properties in enabled_options.items():
-        
-        current_value_string = properties["current_value"]
-        previous_value_string = properties["previous_value"]
 
-        if current_value_string != previous_value_string:
-            current_value_float = float(current_value_string)
-
-        if option == "al30c_buy":
-
-            useful_values[option]    
-            current_value_string = properties["current_value"]
-            previous_value_string = properties["previous_value"]
-
-        else:
-            current_value_float = float(properties["current_value"])
-            useful_values[option.split("_")[0]] = current_value_float / al30c
+def update_prices():
+    update_option_prices(al30_sell)
+    update_option_prices(al30d_sell)
 
 
+def update_option_prices(option):
+    option["previous_price"] = option["current_price"]
+    option["current_price"] = round(
+        option["current_value"] / al30c_buy["current_value"], 4
+    )
 
 
-    # if al30_sell != previous_al30_sell or al30c_buy != previous_al30c_buy:
-    #     al30c = al30_sell / al30c_buy
-    
-    # if al30d_sell != previous_al30d_sell or al30c_buy != previous_al30c_buy:
-    #     al30d = al30d_sell / al30c_buy
-
-    # print(
-    #     datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-    #     + "   "
-    #     + get_icon(al30c, previous_values["al30c"])
-    #     + " "
-    #     + f"{al30c:.3f}"
-    #     + " ("
-    #     + al30_sell_string
-    #     + "/"
-    #     + al30c_buy_string
-    #     + ")   "
-    #     + get_icon(al30d, previous_values["al30d"])
-    #     + " "
-    #     + f"{al30d:.3f}"
-    #     + " ("
-    #     + al30d_sell_string
-    #     + "/"
-    #     + al30c_buy_string
-    #     + ")"
-    # )
+def print_prices():
+    print(
+        datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        + "   "
+        + get_recap(al30_sell)
+        + "    "
+        + get_recap(al30d_sell)
+    )
 
 
+def get_recap(option):
+    return (
+        get_visual_clue(option)
+        + " "
+        + f'{option["current_price"]:.4f}'
+        + " ("
+        + f'{option["current_value"]:.4f}'
+        + "/"
+        + f'{al30c_buy["current_value"]:.4f}'
+        + ")"
+    )
 
 
+def get_visual_clue(option):
+    name = option["name"]
+    current_price = option["current_price"]
+    previous_price = option["previous_price"]
 
-
-def get_icon(first_value, second_value):
-    icon = ""
-    if first_value == second_value:
-        icon = "→"
-    elif first_value < second_value:
-        icon = "↓"    
+    clue = ""
+    if (name == "al30_sell" and current_price >= 1200) or (
+        name == "al30d_sell" and current_price >= 1.015
+    ):
+        clue = "→"
+        winsound.Beep(4250, 100)
+    elif current_price == previous_price or previous_price == 0:
+        clue = " "
+    elif current_price < previous_price:
+        clue = "↓"
     else:
-        icon = "↑"
-
-    return icon
-
-def update_enabled_options_property_previous_value():
-    for properties in enabled_options.values():
-        properties["previous_value"] = properties["current_value"]
+        clue = "↑"
+    return clue
 
 
-
-enabled_options = get_enabled_options()
-
-seconds = int(input("refresh rate in seconds: "))
-print("                           al30c                   al30d")
-
-
-al30_sell_string = "840.00"
-al30c_buy_string = "0.69"
-al30d_sell_string = "0.685"
-
-al30c = 1217.391 
-al30d = 0.993
-
+seconds = 1  # int(input("refresh rate in seconds: "))
+space = " "
+print(space * 29 + "al30" + space * 25 + "al30d")
 while True:
-    update_enabled_options_property_current_value()
-    print_useful_values()
-    update_enabled_options_property_previous_value()
+    update_values()
+    update_prices()
+    print_prices()
     time.sleep(seconds)
